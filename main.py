2c22b3bc57e17cd39b8496e35176b6248f0adb91
luizpaulo73 <luiz.paulo2005@hotmail.com>
2024-11-04 22:26:05 -0300

    finalizando

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
 import oracledb
 
 app = Flask(__name__)
+
+
+@app.after_request
+def apply_cors(response):
+    response.headers["Access-Control-Allow-Origin"] = "http://localhost:3000"  # Permitir o domínio do frontend
+    response.headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS, PUT, DELETE"  # Permitir métodos HTTP necessários
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"  # Permitir cabeçalhos específicos
+    return response
+
+@app.before_request
+def handle_options():
+    if request.method == "OPTIONS":
+        response = make_response()
+        response.headers["Access-Control-Allow-Origin"] = "http://localhost:3000"
+        response.headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS, PUT, DELETE"
+        response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+        return response, 200
 
 # Função para obter a conexão com o banco de dados Oracle
 def get_connection():
@@ -34,18 +51,18 @@ def adicionar_veiculo_db(marca, modelo, tipo_transmissao, ano, placa, cpf):
 
 
 # Função para atualizar um veículo no banco de dados
-def atualizar_veiculo_db(id_veiculo, marca, modelo, tipo_transmissao, ano, placa, cpf):
+def atualizar_veiculo_db(marca, modelo, tipo_transmissao, ano, placa, cpf):
     connection = get_connection()
     cursor = connection.cursor()
     try:
         cursor.execute('''
             UPDATE tbl_veiculo_challenge
             SET marca = :marca, modelo = :modelo, tipo_transmissao = :tipo_transmissao,
-                ano = :ano, placa = :placa, cpf = :cpf
-            WHERE id_veiculo = :id_veiculo
-        ''', {'marca': marca, 'modelo': modelo, 'tipo_transmissao': tipo_transmissao, 'ano': ano, 'placa': placa, 'cpf': cpf, 'id_veiculo': id_veiculo})
+                ano = :ano
+            WHERE placa = :placa AND cpf = :cpf
+        ''', {'marca': marca, 'modelo': modelo, 'tipo_transmissao': tipo_transmissao, 'ano': ano, 'placa': placa, 'cpf': cpf})
         connection.commit()
-        return cursor.rowcount > 0  # Retorna True se algum registro foi atualizado
+        return cursor.rowcount > 0
     except oracledb.DatabaseError as e:
         error, = e.args
         print("Erro ao atualizar veículo:", error.message)
@@ -54,14 +71,15 @@ def atualizar_veiculo_db(id_veiculo, marca, modelo, tipo_transmissao, ano, placa
         cursor.close()
         connection.close()
 
+
 # Função para deletar um veículo no banco de dados
-def deletar_veiculo_db(id_veiculo):
+def deletar_veiculo_db(placa):
     connection = get_connection()
     cursor = connection.cursor()
     try:
-        cursor.execute("DELETE FROM tbl_veiculo_challenge WHERE id_veiculo = :id_veiculo", {'id_veiculo': id_veiculo})
+        cursor.execute("DELETE FROM tbl_veiculo_challenge WHERE placa = :placa", {'placa': placa})
         connection.commit()
-        return cursor.rowcount > 0  # Retorna True se algum registro foi deletado
+        return cursor.rowcount > 0
     except oracledb.DatabaseError as e:
         error, = e.args
         print("Erro ao deletar veículo:", error.message)
@@ -71,14 +89,8 @@ def deletar_veiculo_db(id_veiculo):
         connection.close()
 
 # Endpoint para adicionar um veículo
-@app.route('/veiculos/<string:cpf>', methods=['POST'])
-def adicionar_veiculo(cpf):
-    data = request.json
-    marca = data.get('marca')
-    modelo = data.get('modelo')
-    tipo_transmissao = data.get('tipo_transmissao')
-    ano = data.get('ano')
-    placa = data.get('placa')
+@app.route('/veiculos/<string:cpf>/<string:marca>/<string:modelo>/<string:tipo_transmissao>/<int:ano>/<string:placa>', methods=['POST'])
+def adicionar_veiculo(cpf, marca, modelo, tipo_transmissao, ano, placa):
 
     if not all([marca, modelo, tipo_transmissao, ano, placa, cpf]):
         return jsonify({'message': 'Dados incompletos para adicionar o veículo'}), 400
@@ -90,12 +102,35 @@ def adicionar_veiculo(cpf):
         return jsonify({"message": "Erro ao adicionar veículo: " + str(e)}), 500
 
 # Endpoint para listar todos os veículos
-@app.route('/veiculos/<string:cpf>', methods=['GET'])
-def listar_veiculos(cpf):
+@app.route('/veiculos/<string:cpf>/<string:placa>', methods=['GET'])
+def listar_veiculos(cpf , placa):
     try:
         connection = get_connection()
         cursor = connection.cursor()
-        sql = f"SELECT * FROM tbl_veiculo_challenge WHERE cpf = {cpf}"
+        sql = f"SELECT * FROM tbl_veiculo_challenge WHERE cpf = '{cpf}' and placa = '{placa}'"
+        cursor.execute(sql)
+        tabela = cursor.fetchone()
+        tabela = {
+                "id" : tabela[0],
+                "marca" : tabela[1],
+                "modelo": tabela[2],
+                "tipo_transmissao": tabela[3],
+                "ano": tabela[4],
+                "placa": tabela[5],
+                "cpf": tabela[6]
+            }
+        cursor.close()
+        connection.close()
+        return jsonify(tabela), 200
+    except Exception as e:
+        return jsonify({"message": "Erro ao listar veículos: " + str(e)}), 500
+    
+@app.route('/veiculos/<string:cpf>', methods=['GET'])
+def listar_todos_veiculos(cpf):
+    try:
+        connection = get_connection()
+        cursor = connection.cursor()
+        sql = f"SELECT * FROM tbl_veiculo_challenge WHERE cpf = '{cpf}'"
         cursor.execute(sql)
         tabela = cursor.fetchall()
         lista = []
@@ -110,7 +145,6 @@ def listar_veiculos(cpf):
                 "cpf": item[6]
             }
             lista.append(veiculo)
-        print(lista)
         cursor.close()
         connection.close()
         return jsonify(lista), 200
@@ -118,31 +152,31 @@ def listar_veiculos(cpf):
         return jsonify({"message": "Erro ao listar veículos: " + str(e)}), 500
 
 # Endpoint para atualizar um veículo
-@app.route('/veiculos/<string:cpf>/<int:id_veiculo>', methods=['PUT'])
-def atualizar_veiculo(cpf, id_veiculo):
+@app.route('/veiculos/<string:cpf>/<string:placa>', methods=['PUT'])
+def atualizar_veiculo(cpf, placa):
     data = request.json
     marca = data.get('marca')
     modelo = data.get('modelo')
     tipo_transmissao = data.get('tipo_transmissao')
     ano = data.get('ano')
-    placa = data.get('placa')
 
-    if not all([marca, modelo, tipo_transmissao, ano, placa, cpf]):
+    if not all([marca, modelo, tipo_transmissao, ano]):
         return jsonify({'message': 'Dados incompletos para atualizar o veículo'}), 400
 
     try:
-        atualizado = atualizar_veiculo_db(id_veiculo, marca, modelo, tipo_transmissao, ano, placa, cpf)
+        atualizado = atualizar_veiculo_db(marca, modelo, tipo_transmissao, ano, placa, cpf)
         if not atualizado:
             return jsonify({"message": "Veículo não encontrado"}), 404
         return jsonify({"message": "Veículo atualizado com sucesso"}), 200
     except Exception as e:
         return jsonify({"message": "Erro ao atualizar veículo: " + str(e)}), 500
 
+
 # Endpoint para deletar um veículo
-@app.route('/veiculos/<string:cpf>/<int:id_veiculo>', methods=['DELETE'])
-def deletar_veiculo(cpf, id_veiculo):
+@app.route('/veiculos/<string:placa>', methods=['DELETE'])
+def deletar_veiculo(placa):
     try:
-        deletado = deletar_veiculo_db(id_veiculo)
+        deletado = deletar_veiculo_db(placa)
         if not deletado:
             return jsonify({"message": "Veículo não encontrado"}), 404
         return jsonify({"message": "Veículo deletado com sucesso"}), 200
